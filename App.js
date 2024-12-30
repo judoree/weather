c6052a101fc81afcd4e4e6ed1a70938ac66b46cb
judoree <135936771+judoree@users.noreply.github.com>
2024-12-30 15:19:53 +0900

    Style

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
 import React from "react";
-import { View } from "react-native";
+import { BackHandler, StyleSheet, View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
-      <View style={{ flex: 5, backgroundColor: "teal" }}></View>
-      <View style={{ flex: 1, backgroundColor: "red" }}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.descripiton}>Suny</Text>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 39,
+    fontWeight: "600",
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 158,
+    marginTop: 50,
+  },
+  descripiton: {
+    marginTop: -40,
+    fontSize: 50,
+  },
+});
